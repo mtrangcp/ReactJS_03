21967dd2e0115f46c31ccb273e98824dcb8f0e61
mtrangcp <haminhtrang545@gmail.com>
2025-08-07 07:26:13 +0700

    update

--- src/bai10.ts
+++ src/bai10.ts
@@ -1,26 +1,41 @@
-function isUnique(word: string): boolean {
-  const charSet = new Set<string>();
-  for (let char of word) {
-    if (charSet.has(char)) return false;
-    charSet.add(char);
-  }
-  return true;
-}
+// function isUnique(word: string): boolean {
+//   const charSet = new Set<string>();
+//   for (let char of word) {
+//     if (charSet.has(char)) return false;
+//     charSet.add(char);
+//   }
+//   return true;
+// }
 
-function findLongest(sentence: string): string {
-  const words = sentence.split(" ");
-  let longestWord = "";
+// function findLongest(sentence: string): string {
+//   const words = sentence.split(" ");
+//   let longestWord = "";
 
-  for (let word of words) {
-    if (isUnique(word) && word.length > longestWord.length) {
-      longestWord = word;
+//   for (let word of words) {
+//     if (isUnique(word) && word.length > longestWord.length) {
+//       longestWord = word;
+//     }
+//   }
+
+//   return longestWord;
+// }
+
+function checkSubString(string: string): string {
+  let words: string[] = string.split(" ");
+  let result: string = "";
+  words.forEach((word) => {
+    let charSet = new Set(word);
+
+    if (charSet.size === word.length) {
+      if (charSet.size >= result.length) result = word;
+      
     }
-  }
 
-  return longestWord;
+  });
+    return result;
+
 }
 
 const sentence = "hello world apple banana orange pumpkin cucumber";
-const result = findLongest(sentence);
-console.log(result); 
+console.log(checkSubString(sentence)); 
 
